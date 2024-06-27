e646d706f06a8a83061060a9903260e5d871db01
User <user@Mac-mini-User.local>
2024-06-27 18:23:39 +0300

    dev2: redesign aboutTsx

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -3,32 +3,37 @@ import { styled } from "@mui/material";
 
 const Main = styled("div")({
     minHeight: "80vh",
+    backgroundColor: "rgb(37, 37, 37)",
+    boxShadow: "0px 0px 33px 32px rgba(37, 37, 37, 1)",
 });
 
 const ZagMain = styled("div")({
     font: "small-caps 50px/1 sans-serif",
     marginLeft: "15%",
+    marginTop: "3%",
 });
 const PodZagMain = styled("div")({
     font: "small-caps 35px/1 sans-serif",
-    marginTop: "5%",
-    marginLeft: "20%",
+    marginTop: "3%",
+    marginLeft: "5%",
     maxWidth: "700px",
+    paddingTop: "10px",
+    paddingBottom: "10px",
     textAlign: "justify",
     marginRight: "10%",
-    color: "rgb(211, 211, 211)",
     textIndent: "20px",
+    backgroundColor: "rgb(25, 25, 25)",
+    borderRadius: "40px",
+    boxShadow: "15px 10px 20px 1px rgba(231, 0, 0, 1)",
 });
 const MainText = styled("div")({
     font: "small-caps 25px/1 sans-serif",
-    maxWidth: "500px",
     marginTop: "1%",
     textIndent: "20px",
     textAlign: "justify",
-    marginLeft: "25%",
+    marginLeft: "10%",
     marginRight: "15%",
     marginBottom: "10%",
-    color: "rgb(168, 168, 168)",
 });
 export default function About() {
     return (
@@ -40,40 +45,57 @@ export default function About() {
                     content="Информация о команде и миссии компании"
                 />
             </Head>
+
             <Main>
-                <ZagMain>О нас</ZagMain>
-                <PodZagMain>
-                    OurCompany – это ведущая компания в области индустрия или
-                    сфера деятельности, предлагающая инновационные решения для
-                    ваша целевая аудитория. Мы гордимся тем, что предоставляем
-                    высококачественные продукты и услуги, соответствующие самым
-                    высоким стандартам индустрии.
-                </PodZagMain>
-                <ZagMain>Наши ценности</ZagMain>
-                <PodZagMain>Качество</PodZagMain>
-                <MainText>
-                    Мы стремимся к превосходству в каждом аспекте нашей работы,
-                    гарантируя, что наши клиенты получают только лучшие продукты
-                    и услуги.
-                </MainText>
-                <PodZagMain>Инновации</PodZagMain>
-                <MainText>
-                    Мы постоянно ищем новые способы улучшить наши предложения и
-                    внедряем передовые технологии, чтобы оставаться на шаг
-                    впереди.
-                </MainText>
-                <PodZagMain>Надежность</PodZagMain>
-                <MainText>
-                    Наши клиенты могут рассчитывать на нас в любой ситуации. Мы
-                    поддерживаем тесные и долгосрочные отношения, основанные на
-                    доверии и уважении.
-                </MainText>
-                <PodZagMain>Устойчивое развитие</PodZagMain>
-                <MainText>
-                    Мы заботимся о будущем нашей планеты и стремимся к
-                    устойчивому развитию, минимизируя наш экологический след и
-                    поддерживая экологические инициативы.
-                </MainText>
+                <ZagMain>
+                    О нас
+                    <PodZagMain>
+                        <MainText>
+                            OurCompany – это ведущая компания в области
+                            индустрия или сфера деятельности, предлагающая
+                            инновационные решения для ваша целевая аудитория. Мы
+                            гордимся тем, что предоставляем высококачественные
+                            продукты и услуги, соответствующие самым высоким
+                            стандартам индустрии.
+                        </MainText>
+                    </PodZagMain>
+                </ZagMain>
+
+                <ZagMain>
+                    Наши ценности
+                    <PodZagMain>
+                        Качество
+                        <MainText>
+                            Мы стремимся к превосходству в каждом аспекте нашей
+                            работы, гарантируя, что наши клиенты получают только
+                            лучшие продукты и услуги.
+                        </MainText>
+                    </PodZagMain>
+                    <PodZagMain>
+                        Инновации
+                        <MainText>
+                            Мы постоянно ищем новые способы улучшить наши
+                            предложения и внедряем передовые технологии, чтобы
+                            оставаться на шаг впереди.
+                        </MainText>
+                    </PodZagMain>
+                    <PodZagMain>
+                        Надежность
+                        <MainText>
+                            Наши клиенты могут рассчитывать на нас в любой
+                            ситуации. Мы поддерживаем тесные и долгосрочные
+                            отношения, основанные на доверии и уважении.
+                        </MainText>
+                    </PodZagMain>
+                    <PodZagMain>
+                        Устойчивое развитие
+                        <MainText>
+                            Мы заботимся о будущем нашей планеты и стремимся к
+                            устойчивому развитию, минимизируя наш экологический
+                            след и поддерживая экологические инициативы.
+                        </MainText>
+                    </PodZagMain>
+                </ZagMain>
             </Main>
         </div>
     );
